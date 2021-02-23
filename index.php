d18d3b351577f5e6006b3f5137494114dbf6884b
tarqhilmarsiregar <tarqhilmarsiregar@gmail.com>
2021-02-23 07:19:21 +0700

    Menambahkan file index.php dan index.js

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar Git Remote 4</title>
+</head>
+<body>
+    <h3>Hello World!</h3>
+</body>
+</html>
+
+?>
